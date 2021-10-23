7fda7252e17a2fa55e9987e1e52f4526954cb302
neo <62314994+NeyoCreator@users.noreply.github.com>
2021-10-23 15:53:09 +0200

    Added notes and extraction script

---
+++ Step_2/2.3 Bank_Statement_Analyses/extractData.py
@@ -0,0 +1,23 @@
+# Aim : to extract data from the image bank statement
+
+#importing the libraries
+import cv2
+import numpy as np
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+import matplotlib.pyplot as plt
+import pandas as pd
+from PIL import Image
+
+#create a function that wwill return a threshold image and the value 
+
+def extract_data(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    threshed = cv2.adaptiveThreshold(gray,255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,17)
+    text1 = tess.image_to_data(threshed,output_type='data.frame')
+    text2 = tess.image_to_string(threshed)
+    text = text1[text1.conf != -1]
+    lines = text.groupby('block_num')['text'].apply(list)
+    conf = text.groupby(['block_num'])['conf'].mean()
+
+    return text2,threshed
