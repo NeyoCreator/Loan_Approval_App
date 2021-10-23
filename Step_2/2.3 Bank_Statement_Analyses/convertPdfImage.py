ffb955f4513f793f463fdb191688e40d34ccd266
neo <62314994+NeyoCreator@users.noreply.github.com>
2021-10-23 15:37:58 +0200

    Managed to convert PDF to image

--- Step_2/2.3 Bank_Statement_Analyses/convertPdfImage.py
+++ Step_2/2.3 Bank_Statement_Analyses/convertPdfImage.py
@@ -1,16 +1,9 @@
 from pdf2image import convert_from_path
 
-pdf_file = "first_page.pdf"
-pages = convert_from_path(pdf_file, 500)
+# Store Pdf with_convert_from_path function 
+image = convert_from_path('first_Page.pdf')
 
-image_counter = 1
-
-for page in pages:
-
-    filename = "Retrieve/page_" _+str(image_counter) + ".jpg"
-
-    # save the image of the page in the system
-    page.save(filename, "JPEG")
-    
-    # increment the counter to update filename
-    image_counter = image_counter + 1
+for i in range(len(image)):
+   
+      # Save pages as images in the pdf
+    image[i].save('page'+ str(i) +'.jpg', 'JPEG')
