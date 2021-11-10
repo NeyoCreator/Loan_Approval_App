fbc5fa732f798cebbcf7fc27fd9ef10c09e0d594
NeyoCreator <neos25722@gmail.com>
2021-11-11 01:50:49 +0200

    Added face recognition model to the application

--- prototype/part1.py
+++ prototype/part1.py
@@ -13,6 +13,7 @@ import dlib
 import numpy as np
 import tkinter as tk 
 from tkinter import  messagebox  
+from PIL import ImageTk, Image
 
 #BEGIN
 
@@ -29,7 +30,7 @@ global conn
 # Create part1 window
 root = Tk()
 root.title("Sign up")
-root.geometry("400x400")
+root.geometry("250x250")
 
 # Window that takes face image
 def face_image():
@@ -49,6 +50,7 @@ def face_image():
         #annotate rectangle 
         cv2.rectangle(frame, (80,80), (450,450), (0,30,255), thickness= 2, lineType=cv2.LINE_AA)
         cv2.imshow("Main frame thing", frame)
+    
 
         # cv2.waitKey(0)
         key = cv2.waitKey(1)
@@ -63,8 +65,9 @@ def face_image():
             video.release()
             cv2.destroyAllWindows()
             break
-
-
+    
+    
+    
         
 # Window that take card image 
 def id_card():
@@ -111,27 +114,81 @@ def convertToBinaryData(filename):
 
 # Load variables to dataframe
 def submit():
-    #convert images 
-    image_face = convertToBinaryData(face_image.variable)
-    image_id = convertToBinaryData(id_card.variable)
+    # 1.Convert images 
+    image_face_blob = convertToBinaryData(face_image.variable)
+    image_id_blob = convertToBinaryData(id_card.variable)
 
-    data_tuple = (email,password,image_face,image_id)
 
-    # Create database
-    conn = sqlite3.connect('user_profile.db')
-    # create cursor
-    c = conn.cursor()
-    sqlite_insert_blob_query = """INSERT INTO users(
-        email,    
-        password,
-        image_face,
-        image_id) values(?,?,?,?)"""
+    #load the image 
+    image_face = dlib.load_rgb_image(face_image.variable)
+    image_id = dlib.load_rgb_image(id_card.variable)
 
-    c.execute(sqlite_insert_blob_query,data_tuple)    
-    # Commit Changes 
-    conn.commit()
-    # Close connection
-    conn.close()
+    # 2.Create tuple for all values
+    data_tuple = (email,password,image_face_blob,image_id_blob)
+
+    # 3.Match the Images
+    detector = dlib.get_frontal_face_detector()
+    sp = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
+    model = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+
+    # 4.Apply face detection  using detector object 
+    img1_detect = detector(image_face,1)
+    img2_detect = detector(image_id,1)
+
+    # 5.Get the image shapes 
+    img1_shape = sp(image_face,img1_detect[0])
+    img2_shape = sp(image_id,img2_detect[0])
+
+    # 6.Align the images 
+    img1_align = dlib.get_face_chip(image_face, img1_shape)
+    img2_align = dlib.get_face_chip(image_id, img2_shape)
+
+    #implement the model 
+    img1_rep = model.compute_face_descriptor(img1_align)
+    img2_rep = model.compute_face_descriptor(img2_align)
+
+    #the representation of the images are dlib.vectors we need to convert them into numpy arrays
+    img1_rep = np.array(img1_rep)
+    img2_rep = np.array(img2_rep)
+
+    """the final step is verification we need to find the distance between the arrays
+        the greater the distance the less similar the images are to each other.
+        Create a function that find the euclidian distance between the images """
+
+    def find_distance(img_x1, img_x2):
+        euclidian_distance = img_x1 - img_x2
+        euclidian_distance = np.sum(np.multiply(euclidian_distance,euclidian_distance))
+        euclidian_distance = np.sqrt(euclidian_distance)
+        return euclidian_distance
+
+    distance = find_distance(img1_rep,img2_rep)
+
+
+    if distance < 0.6 :
+        # Show results 
+        result = "Images match"
+        label_result = Label(part2, text=result)
+        label_result.grid(row=3, column=0)
+
+        # Create database
+        conn = sqlite3.connect('user_profile.db')
+        # 2.Create cursor
+        c = conn.cursor()
+        sqlite_insert_blob_query = """INSERT INTO users(
+            email,    
+            password,
+            image_face,
+            image_id) values(?,?,?,?)"""
+
+        c.execute(sqlite_insert_blob_query,data_tuple)    
+        # Commit Changes 
+        conn.commit()
+        # Close connection
+        conn.close()
+    else :
+        result = "Images do not match"
+        label_result = Label(part2, text=result)
+        label_result.grid(row=3, column=0)
 
     
 # Sign up user to application
@@ -139,6 +196,7 @@ def sign_up():
     # Set global variables
     global email
     global password
+    global part2
 
     conn = sqlite3.connect('user_profile.db')
     # create cursor
@@ -162,22 +220,28 @@ def sign_up():
 
     # Open new window
     part2 = Toplevel()
-    Button(part2, text ="Take Image", command= face_image).pack()
-    Button(part2, text ="Take ID card", command= id_card).pack()
-    Button(part2, text ="Submit",command = submit).pack()
+    part2.title("Personal Details")
+    part2.geometry("250x250")
+    btn_image = Button(part2, text ="Take Image", command= face_image)
+    btn_image.grid(row=0, column=0, columnspan=3, padx=10,pady=10)
+    btn_card = Button(part2, text ="Take ID card", command= id_card)
+    btn_card.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
+    btn_submit= Button(part2, text ="Submit",command = submit)
+    btn_submit.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
 
 #email_entry
-mail_ent = Entry(root,)
-mail_ent.pack()
+mail_ent = Entry(root,width = 35,borderwidth=5)
+mail_ent.grid(row=0, column=0,columnspan=3,padx=10,pady=10)
 mail_ent.insert(0,"e-mail: ")
 
 #password_entry
-pass_ent = Entry(root,)
-pass_ent.pack()
+pass_ent = Entry(root,width = 35,borderwidth=5)
+pass_ent.grid(row=1, column=0,columnspan=3,padx=10,pady=10)
 pass_ent.insert(0,"password: ")
 
 myButton = Button(root, text ="Continue", command=sign_up)
-myButton.pack()
+myButton.grid(row=2, column=0,columnspan=3,padx=10,pady=10)
+
 
 
 root.mainloop()
