dfa5b44ee0087ac47e2fe604fc116bbc90ce149d
NeyoCreator <neos25722@gmail.com>
2021-11-10 04:24:12 +0200

    added a tkinter prototype

--- Step_2/2.3 Bank_Statement_Analyses/extractData.py
+++ Step_2/2.3 Bank_Statement_Analyses/extractData.py
@@ -1,4 +1,4 @@
-# Aim : to extract data from the image bank statement
+# Aim :to extract data from the image bank statement
 
 #importing the libraries
 import cv2
@@ -10,15 +10,14 @@ import pandas as pd
 from PIL import Image
 
 #create a function that wwill return a threshold image and the value 
-
 def extract_data(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     threshed = cv2.adaptiveThreshold(gray,255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,17)
     text1 = tess.image_to_data(threshed,output_type='data.frame')
     text2 = tess.image_to_string(threshed)
     text = text1[text1.conf != -1]
-    lines = text.groupby('block_num')['text'].apply(list)
-    conf = text.groupby(['block_num'])['conf'].mean()
+    # lines = text.groupby('block_num')['text'].apply(list)
+    # conf = text.groupby(['block_num'])['conf'].mean()
     return text2
 
 # implement the function 
@@ -28,4 +27,4 @@ txt_value = extract_data(img)
 # split the string to array
 arrValue = txt_value.split("\n")
 
-print(arrValue[2])
+##print(arrValue[2])
