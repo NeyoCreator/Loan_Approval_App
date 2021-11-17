8d53baeb5541a6748651322edb30d45b71941c36
NeyoCreator <neos25722@gmail.com>
2021-11-17 07:32:49 +0200

    Added Experiment folder

--- Step_2/2.3 Bank_Statement_Analyses/convertPdfImage.py
+++
@@ -1,9 +0,0 @@
-from pdf2image import convert_from_path
-
-# Store Pdf with_convert_from_path function 
-image = convert_from_path('second_page.pdf')
-
-for i in range(len(image)):
-   
-      # Save pages as images in the pdf
-    image[i].save('page'+ str(i) +'.jpg', 'JPEG')
